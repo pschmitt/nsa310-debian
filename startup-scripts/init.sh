49d2fffdef41cb5ec8e629c0cfb0dc35a7bfbb64
Philipp Schmitt <philipp@schmitt.co>
2013-12-01 01:37:59 +0100

    Update init.sh
    Update debian path

--- startup-scripts/init.sh
+++ startup-scripts/init.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 case "$1" in
         start|startup)
-                mount --bind /proc /etc/zyxel/storage/sysvol/.debian/root/proc
-                mount --bind /sys /etc/zyxel/storage/sysvol/.debian/root/sys
-                mount -t devpts none /etc/zyxel/storage/sysvol/.debian/root/dev/pts
-                mount --bind /etc/zyxel/storage/sysvol /etc/zyxel/storage/sysvol/.debian/root/data
-                cp /etc/resolv.conf /etc/zyxel/storage/sysvol/.debian/root/etc
-                chroot /etc/zyxel/storage/sysvol/.debian/root /etc/chroot-init $1
+                mount --bind /proc /etc/zyxel/storage/sysvol/debian/root/proc
+                mount --bind /sys /etc/zyxel/storage/sysvol/debian/root/sys
+                mount -t devpts none /etc/zyxel/storage/sysvol/debian/root/dev/pts
+                mount --bind /etc/zyxel/storage/sysvol /etc/zyxel/storage/sysvol/debian/root/data
+                cp /etc/resolv.conf /etc/zyxel/storage/sysvol/debian/root/etc
+                chroot /etc/zyxel/storage/sysvol/debian/root /etc/chroot-init $1
                 ;;
         stop|shutdown)
-                chroot /etc/zyxel/storage/sysvol/.debian/root /etc/chroot-init $1
-                umount /etc/zyxel/storage/sysvol/.debian/root/proc
-                umount /etc/zyxel/storage/sysvol/.debian/root/sys
-                umount /etc/zyxel/storage/sysvol/.debian/root/dev/pts
-                umount /etc/zyxel/storage/sysvol /etc/zyxel/storage/sysvol/.debian/root/data
+                chroot /etc/zyxel/storage/sysvol/debian/root /etc/chroot-init $1
+                umount /etc/zyxel/storage/sysvol/debian/root/proc
+                umount /etc/zyxel/storage/sysvol/debian/root/sys
+                umount /etc/zyxel/storage/sysvol/debian/root/dev/pts
+                umount /etc/zyxel/storage/sysvol /etc/zyxel/storage/sysvol/debian/root/data
                 ;;
         *)
                 exit 0
